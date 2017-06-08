783b8911683f18286080ea68db348159220934df
aobando98 <alberto.obando98@gmail.com>
2017-06-08 15:57:54 -0600

    Decks and Api

---
+++ src/decks/Challenge.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decks;
+
+/**
+ *
+ * @author Alberto Obando
+ */
+public class Challenge {
+    
+    private int Times;
+    private String Place;
+    
+}
---
+++ src/decks/City.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decks;
+
+/**
+ *
+ * @author Alberto Obando
+ */
+public class City {
+    
+    private String Name;
+    private int Longitude;
+    private int Latitude;
+    private int Radio;
+    
+}
