a4c7c9c412aaf18a015a1626797420f9e4a3bf26
aobando98 <albertoobando@Albertos-MacBook-Pro.local>
2017-06-16 21:44:35 -0600

    More changes over Dijkstra
    The algorithm now calculates correctly the min weight to get to all the
    nodes

--- src/graph/Dijkstra.java
+++ src/graph/Dijkstra.java
@@ -14,7 +14,7 @@ import java.util.Map.Entry;
  */
 public class Dijkstra {
     
-    private ArrayList<GraphNode> ultimo;
+    private ArrayList<Arc> References; //List to know from which node the min distance comes from
     private ArrayList<GraphNode> Nodes; //The list of nodes in the graph
     private ArrayList<Boolean> MinDistanceStatus; //This list is used to know if we have the lowest weight for a given Node
     private ArrayList<Double> MinDistance; //This is the list that will contain the minimum distance to get to that node
@@ -22,12 +22,27 @@ public class Dijkstra {
     private GraphNode Source; //Node to calculate the minimum distance
     
     public Dijkstra(Graph gp, GraphNode origen){ 
-        AmountNodes = gp.getArcs().size(); 
+        AmountNodes = gp.getNodes().size(); 
         Source = origen;
         Nodes = gp.getNodes(); //List of Nodes that belong to the graph
-        MinDistanceStatus = new ArrayList<Boolean>(AmountNodes);
-        MinDistance = new ArrayList<Double>(AmountNodes);
+        MinDistanceStatus = new ArrayList<>(gp.getNodes().size());
+        MinDistance = new ArrayList<>();
+        References = new ArrayList<>(AmountNodes);
+        this.setMax();  //initializez values
+        this.setFalse(); //initializez values
+        this.setNull(); //initializes values
     }
+    
+    //Getters y Setters
+
+    public ArrayList<Arc> getReferences() {
+        return References;
+    }
+
+    public void setReferences(ArrayList<Arc> References) {
+        this.References = References;
+    }
+    
     
     /**
      * Function to get the lowest weight of a node path
@@ -48,7 +63,7 @@ public class Dijkstra {
     }
     
     /**
-     * Function to get the node of the lowest weight path
+     * Function to get the Destiny node of the lowest weight path
      * @param pNode
      * @param pWeight
      * @return Destiny Node of the lowest weight
@@ -63,6 +78,47 @@ public class Dijkstra {
             if (conections.get(i).getWeight() + pWeight < max_double ){
                 max_double = conections.get(i).getWeight(); //updates the min weight
                 result = conections.get(i).getDestiny(); //returns the destiny Node
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * Function to get the position of the min non final distance we have until now
+     * @return position of the min non final distance
+     */
+    public int getMinDistanceArray(){
+        int result = 0;
+        double max_double = Double.MAX_VALUE;
+        
+        for (int i = 0; i < MinDistance.size(); i++){
+            if (!MinDistanceStatus.get(i)){ //only checks for non final weights
+                if (MinDistance.get(i) < max_double ){
+                    max_double = MinDistance.get(i);
+                    result = i;
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Function to get the arc of the min distance
+     * @param pNode
+     * @param pWeight
+     * @return arc of the min distance
+     */
+    public Arc getMinDistanceArc(GraphNode pNode, double pWeight){
+        Arc result = null;
+        double max_double = Double.MAX_VALUE;
+        
+        ArrayList<Arc> conections = pNode.getConections();
+        
+        for (int i = 0; i < conections.size(); i++){
+            if (conections.get(i).getWeight() + pWeight < max_double ){
+                max_double = conections.get(i).getWeight(); //updates the min weight
+                result = conections.get(i);
             }
         }
         return result;
@@ -90,8 +146,26 @@ public class Dijkstra {
      * Used mainly to start the algorithm
      */
     public void setFalse(){
-        for (int i = 0; i < MinDistanceStatus.size(); i++){
-            MinDistanceStatus.set(i, false);
+        for (int i = 0; i < Nodes.size(); i++){
+            this.MinDistanceStatus.add(false);
+        }
+    }
+    
+    /**
+     * Sets all the values of the min distance array to its maximum value
+     */
+    public void setMax(){
+        for (int i = 0; i < Nodes.size(); i++){
+            this.MinDistance.add(Double.MAX_VALUE);
+        }
+    }
+    
+    /**
+     * Sets all references to other nodes as null
+     */
+    public void setNull(){
+        for (int i = 0; i < Nodes.size(); i++){
+            this.References.add(null);
         }
     }
     
@@ -99,15 +173,69 @@ public class Dijkstra {
      * Function to get the shortest path between two nodes
      * @param pNodeSource
      * @param pNodeDestiny
-     * @return 
+     * @return List of arcs with the shortest path
      */
     public ArrayList<Arc> getPath(GraphNode pNodeSource, GraphNode pNodeDestiny){
         ArrayList<Arc> result = new ArrayList<Arc>();
         
+        int pos_destiny = findNode(pNodeDestiny); //Returns the position of the destiny node -> used to get the arc of the shortest path
+        GraphNode temp_node = Nodes.get(pos_destiny);
+        while (!temp_node.equals(pNodeSource)){ //until the source node equals the one we want to find
+            result.add(References.get(pos_destiny)); //adds the arc to the reference list -> result list
+            temp_node = References.get(pos_destiny).getSource(); //returns the source of the arc -> so we can find our source node
+        }
+        
+        
         return result;
     }
     
+    /**
+     * Function to get the shortest path from one node to all the others
+     */
+    public void execute(){
+        
+        double default_weight = 0; //This is the weight we change each time we process a new Node
+        GraphNode default_node = Source; //we begin with source
+        MinDistance.set(findNode(Source), 0.0); //sets the weight of the source node to 0
+        MinDistanceStatus.set(findNode(Source), true); //sets the weight as final
+        References.set(findNode(Source), new Arc(0.0, Source, Source)); //sets the source of the as himself so no null pointer exception occurs
+        
+        for (int i = 0; i < Nodes.size(); i++){ //We execute the cicle the number of nodes we have
+            
+            setWeights(default_node, default_weight); //updates the weight of the weight list
+            int min_weight_pos = getMinDistanceArray(); //returns the pos of the min weight
+            double min_weight = MinDistance.get(min_weight_pos); //returns the min weight
+            //GraphNode min_node = getMinDistanceNode(default_node, default_weight); //returns the destiny node of the min weight
+            GraphNode min_node = Nodes.get(min_weight_pos);  //Si no sirve Probar esta linea y no la anterior
+            int pos_node = findNode(min_node); //returns the pos of the destiny node
+            
+            MinDistanceStatus.set(min_weight_pos, true); //sets the weight as final REVISAR
+            
+            default_weight = min_weight; //Change of values to continue the cicle
+            default_node = min_node; //Change of values to continue the 
+            
+        }
+        
+    }
     
+    /**
+     * Function to set the parcial weight of the nodes that aren't final and the Arc to get to it
+     * @param pNode
+     * @param pWeight 
+     */
+    public void setWeights(GraphNode pNode, double pWeight){
+        ArrayList<Arc> conections = pNode.getConections();
+        
+        for (int i = 0; i < conections.size(); i++){
+            GraphNode destiny_node = conections.get(i).getDestiny();
+            GraphNode source_node = conections.get(i).getSource();
+            int pos_node = findNode(destiny_node); //returns the pos of the destiny node
+            if (!MinDistanceStatus.get(pos_node)){ //if the weight isn't final we change it
+                MinDistance.set(pos_node, conections.get(i).getWeight() + pWeight); // we change the min weight of the node
+                References.set(pos_node, conections.get(i)); //Sets the Arc to get to that node
+            }
+        }
+    }
     
     
     
---
+++ src/graph/DijkstraTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
+
+import java.util.*;
+
+/**
+ *
+ * @author albertoobando
+ */
+public class DijkstraTest {
+    
+    private static ArrayList<Arc> Arcs = new ArrayList<Arc>();
+    private static ArrayList<GraphNode> Nodes = new ArrayList<GraphNode>();
+    
+    public static void addLane(int pSource, int pDestiny, double pWeight){
+        GraphNode Source_node = Nodes.get(pSource);
+        GraphNode Destiny_node = Nodes.get(pDestiny);
+        Arc temp_arc = new Arc(pWeight, Source_node, Destiny_node); //Creates the arc
+        Source_node.getConections().add(temp_arc); //adds the reference to the arc
+        Arcs.add(temp_arc); //adds the arc to the arc list of the graph
+    }
+    
+    public static void main(String[] args){
+        
+        //Creates the array of nodes
+        for (int i = 0; i < 8; i++){
+            GraphNode new_node = new GraphNode("Node_" + i, "Link");
+            Nodes.add(new_node);
+        }
+        //Node 0-A
+        addLane(0, 1, 8.0);
+        addLane(0, 3, 5.0);
+        addLane(0, 2, 2.0);
+        //Node 1-B
+        addLane(1, 0, 8.0);
+        addLane(1, 3, 2.0);
+        addLane(1, 5, 13.0);
+        //Node 2-C
+        addLane(2, 0, 2.0);
+        addLane(2, 3, 2.0);
+        addLane(2, 4, 5.0);
+        //Node 3-D
+        addLane(3, 1, 2.0);
+        addLane(3, 2, 2.0);
+        addLane(3, 4, 1.0);
+        addLane(3, 6, 3.0);
+        addLane(3, 5, 6.0);
+        //Node 4-E
+        addLane(4, 2, 5.0);
+        addLane(4, 3, 1.0);
+        addLane(4, 6, 1.0);
+        //Node 5-F
+        addLane(5, 1, 13.0);
+        addLane(5, 3, 6.0);
+        addLane(5, 6, 2.0);
+        addLane(5, 7, 3.0);
+        //Node 6-G
+        addLane(6, 4, 1.0);
+        addLane(6, 3, 3.0);
+        addLane(6, 5, 2.0);
+        addLane(6, 7, 6.0);
+        //Node 7-H
+        addLane(7, 5, 3.0);
+        addLane(7, 6, 6.0);
+        
+        Graph test_graph = new Graph(Nodes, Arcs);
+        Dijkstra test_dijkstra = new Dijkstra(test_graph, Nodes.get(0));
+        test_dijkstra.execute(); //Calculates the shortest path
+        ArrayList<Arc> test_path = test_dijkstra.getPath(Nodes.get(0), Nodes.get(5));
+        for (int x = test_dijkstra.getReferences().size() - 1; x > 0; x--){
+            System.out.println(test_dijkstra.getReferences().get(x).toString());
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+}
